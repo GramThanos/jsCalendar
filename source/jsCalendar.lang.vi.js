973c49c9a3ca25b78071bf6dec64572416be5b2d
gramthanos <gramthanos@gmail.com>
2023-12-12 10:57:31 +0200

    Added Vietnamese Language support #64
    by @dienht

---
+++ source/jsCalendar.lang.vi.js
@@ -0,0 +1,59 @@
+/*
+ * jsCalendar language extension
+ * Add Vietnamese Language support
+ * Translator: Nguyen Duy Dien (dienht@github)
+ */
+
+// We love anonymous functions
+(function(){
+
+    // Get library
+    var jsCalendar = window.jsCalendar;
+
+    // If jsCalendar is not loaded
+    if (typeof jsCalendar === 'undefined') {
+        // If there is no language to load array
+        if (typeof window.jsCalendar_language2load === 'undefined') {
+            window.jsCalendar_language2load = [];
+        }
+        // Wrapper to add language to load list
+        jsCalendar = {
+            addLanguage : function (language) {
+                // Add language to load list
+                window.jsCalendar_language2load.push(language);
+            }
+        };
+    }
+
+    // Add a new language
+    jsCalendar.addLanguage({
+        // Language code
+        code : 'vi',
+        // Months of the year
+        months : [
+            'Tháng 1',
+            'Tháng 2',
+            'Tháng 3',
+            'Tháng 4',
+            'Tháng 5',
+            'Tháng 6',
+            'Tháng 7',
+            'Tháng 8',
+            'Tháng 9',
+            'Tháng 10',
+            'Tháng 11',
+            'Tháng 12',
+        ],
+        // Days of the week
+        days : [
+            'CN',
+            'T2',
+            'T3',
+            'T4',
+            'T5',
+            'T6',
+            'T7'
+        ]
+    });
+
+})();
